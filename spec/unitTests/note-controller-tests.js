0250765a171f558c6c8e6cad0fbb6cd43e24f23e
Corina <fluturelcorina@gmail.com>
2017-07-19 15:32:29 +0100

    TDD-ing code that shows a note in the index

--- spec/unitTests/note-controller-tests.js
+++ spec/unitTests/note-controller-tests.js
@@ -1,12 +1,19 @@
 var noteList = new NoteList();
 var noteController = new NoteController(noteList);
+noteList.createNote("Testinggggg");
+var htmlResult = "<ul><li><div>Testinggggg</div></li></ul>";
 
 it("intializes with a notelist model", function () {
   assertEquals(noteController.noteList, noteList);
 });
 
 it("can see an added note", function () {
-  noteList.createNote("Testinggggg");
-  var htmlResult = "<ul><li><div>Testinggggg</div></li></ul>";
   assertEquals(noteController.noteListView.createHtmlList(), htmlResult);
+});
+
+it("sets the innerHtml property", function() {
+  var dummyElement = document.createElement('div');
+  dummyElement.innerHTML = 'Parteeeeee';
+  noteController.setHtml(dummyElement);
+  assertEquals(dummyElement.innerHTML, htmlResult);
 });
