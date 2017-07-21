0345914f3dec7abaf7fd06bb75a05b0e9c8e8d2a
Corina <fluturelcorina@gmail.com>
2017-07-21 08:50:15 +0100

    Extra work

--- controllers/index.js
+++ controllers/index.js
@@ -2,11 +2,12 @@
 window.onload = function() {
   var list = document.getElementById('app')
   var noteList = new NoteList();
-  noteList.createNote("This is a long line, sooooooo long");
-  noteList.createNote("TGI Friday, yeahhhhhh!");
-  noteList.createNote("Buy more ice cream, BUY ALL THE ICE CREAM!");
+  // noteList.createNote("This is a long line, sooooooo long");
+  // noteList.createNote("TGI Friday, yeahhhhhh!");
+  // noteList.createNote("Buy more ice cream, BUY ALL THE ICE CREAM!");
   var noteController = new NoteController(noteList);
   noteController.setHtml(list)
+  
   
   makeUrlChange();
   
@@ -39,6 +40,20 @@ window.onload = function() {
     document
     .getElementById("app")
     .innerHTML = noteView.getHtml();
+  };
+  
+  submitForm();
+  
+  function submitForm() {
+    window.addEventListener("submit", function(event) {
+      var textInput = document.getElementById("input-text").value;
+      noteList.createNote(textInput);
+      noteController.setHtml(list)
+      document.getElementById("input-text").value = '';
+      // console.log(event.target["note-text"].value);
+      event.preventDefault();
+      
+    })
   };
   
 }
